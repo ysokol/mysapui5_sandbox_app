763242661569d719b2ea8e33acf96240b626edeb
S0004431717 <sokolovskij-yurij@yandex.ru>
2018-04-27 20:55:53 +0000

    3

---
+++ webapp/utils/WorkflowService.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+	"sap/ui/base/Object",
+	"mysapui5_sandbox_app/utils/MyException"
+], function(Object, MyException) {
+	"use strict";
+	return Object.extend("mysapui5_sandbox_app.utils.WorkflowService", {
+
+		constructor: function() {
+			
+		},
+
+		getScript: function(sUrl) {
+			return new Promise(function(resolve, reject) {
+				$.getScript(sUrl, function(sScript, sTextStatus, oJqXhr) {
+					resolve(sScript, sTextStatus, oJqXhr);
+				});	
+			});
+		}
+
+	});
+});
